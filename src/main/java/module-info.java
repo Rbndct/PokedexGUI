505d7ec48241079548cb360d9cc594e4b23e02e5
Rbndct <rbenedict.maagma@gmail.com>
2025-07-20 17:26:18 +0800

    Update module-info.java to include JFoenix dependency and restructure package exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,13 @@ module com.rbee.pokedexgui {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.jfoenix;
 
-    exports com.rbee.pokedexgui.main;
-    opens com.rbee.pokedexgui.main to javafx.fxml;
+    exports com.rbee.pokedexgui;
 
+    exports com.rbee.pokedexgui.controller.loading;
+    opens com.rbee.pokedexgui.controller.loading to javafx.fxml;
+
+    exports com.rbee.pokedexgui.controller.main;
+    opens com.rbee.pokedexgui.controller.main to javafx.fxml;
 }

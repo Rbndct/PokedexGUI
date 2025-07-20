81732ecdea3a051269a1a089e9b8cf8f25be83d3
Rbndct <rbenedict.maagma@gmail.com>
2025-07-20 17:26:02 +0800

    Rename Main class to MainApp and update FXML resource paths; add MainView layout

--- src/main/java/com/rbee/pokedexgui/MainApp.java
+++ src/main/java/com/rbee/pokedexgui/MainApp.java
@@ -1,26 +1,33 @@
-package com.rbee.pokedexgui.main;
+package com.rbee.pokedexgui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class Main extends Application {
+public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/rbee/pokedexgui/fxml/loading_screen.fxml"));
+        Font.loadFont(getClass().getResourceAsStream("/com/rbee/pokedexgui/fonts/PressStart2P-Regular.ttf"), 12);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/rbee/pokedexgui/view/loading/LoadingScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 960, 540);
-        stage.setTitle("Enhanced Pokédex GUI");
+
+        stage.setTitle("Enhanced Pokédex by Rbee :>");
         stage.setScene(scene);
 
         // Set window icon (top-left icon)
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/rbee/pokedexgui/images/pokemon-logo.jpg"))));
 
         stage.show();
+
+        Font.getFamilies().forEach(System.out::println);
+
 
     }
     public static void main(String[] args) {

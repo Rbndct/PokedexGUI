149327d0f9f36a3074cb5382f9d6771a76e7fb05
Rbndct <rbenedict.maagma@gmail.com>
2025-07-26 03:06:45 +0800

    Update module-info.java and pom.xml for package restructuring and main application class adjustment

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,19 @@ module com.rbee.pokedexgui {
     requires java.desktop;
     requires com.jfoenix;
 
-    exports com.rbee.pokedexgui;
+    // Export main application package (assuming your main app class is here)
+    exports com.rbee.pokedexgui.app;
 
+    // Export and open controller packages to javafx.fxml for FXML loading
     exports com.rbee.pokedexgui.controller.loading;
     opens com.rbee.pokedexgui.controller.loading to javafx.fxml;
 
     exports com.rbee.pokedexgui.controller.main;
     opens com.rbee.pokedexgui.controller.main to javafx.fxml;
+
+    exports com.rbee.pokedexgui.controller.pokemon;
+    opens com.rbee.pokedexgui.controller.pokemon to javafx.fxml;
+
+    exports com.rbee.pokedexgui.controller.move;
+    opens com.rbee.pokedexgui.controller.move to javafx.fxml;
 }
